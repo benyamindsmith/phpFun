c749e177c41a885c7c3100617621491ff38d80a0
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2020-12-21 23:53:59 -0500

    Create helloWorld.php

---
+++ helloWorld.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Major Haaak</title>
+    <h1> Major Haak</h1>
+    <?php
+    $name="Yankee";
+    $beverage="milk";
+    echo"Hello $name <br>";
+    echo "Do you like $beverage?<br>";
+     ?>
+  </head>
+  <body>
+
+  </body>
+</html>
